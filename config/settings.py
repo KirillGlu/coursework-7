2d512640ad7b5f2f4bb6c833517e13dd5cc94823
Kirill_Glushankov <kiglu7@gmail.com>
2024-02-19 19:29:23 +0500

    Поправил docker-compose.yml

--- config/settings.py
+++ config/settings.py
@@ -30,9 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-DOMAIN_NAME = os.getenv('DOMAIN_NAME')
+# DOMAIN_NAME = os.getenv('DOMAIN_NAME')
 
-ALLOWED_HOSTS = [urlsplit(DOMAIN_NAME).hostname]
+ALLOWED_HOSTS = []
 
 
 
